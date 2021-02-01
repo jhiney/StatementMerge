cf060e147e592ad49d6e5ecedd4edd8666e8721c
Jake Hiney <hiney.jake@outlook.com>
2021-02-01 08:12:31 -0600

    Update Program.cs

--- StatementMerge/Program.cs
+++ StatementMerge/Program.cs
@@ -10,14 +10,14 @@ using Outlook = Microsoft.Office.Interop.Outlook;
 
 namespace StatementMerge
 {
-    //Things I need to change when this runs each quarter:
-    //The file path in the GetPath() and GetPaths() methods, should be 2 changes.
-    //Change the subject & body in both createMailItem methods so that it reflects the accurate as of date.
-
-    //TODO: add a console readline to read in the "as of date"
-    //Create variable for that paths so I only have to change it once
-    //Have it skip the accounts in the CSV that do not have a match and output them to the console
-    class Program
+	//Things I need to change when this runs each quarter:
+	//The file path in the GetPath() and GetPaths() methods, should be 2 changes.
+	//Change the subject & body in both createMailItem methods so that it reflects the accurate as of date.
+	
+	//TODO: add a console readline to read in the "as of date"
+	//Create variable for that paths so I only have to change it once
+	//Have it skip the accounts in the CSV that do not have a match and output them to the console
+	class Program
 	{
 		[STAThread]
 		static void Main(string[] args)
@@ -51,7 +51,7 @@ namespace StatementMerge
 				string recip = dataRow["name"].ToString();
 				string email = dataRow["E-mail Address"].ToString();
 				string entity = dataRow["Account Name"].ToString();
-				string designation= (dataRow["Desig"].ToString());
+				string designation= dataRow["Desig"].ToString();
 
 				if (designation.Length < 5)
 				{
@@ -233,6 +233,7 @@ namespace StatementMerge
 				string fullName = foundFile.FullName;
 				path = fullName;
 				return path;
+				//Console.WriteLine(fullName);
 			}
 			return path;
 		}
